5731c9f0f4f5d572850b46daaffdd03c12b30b82
matthewkuchurean <matthew.kuchurean@flemingcollege.ca>
2023-01-18 13:49:07 -0500

    Revert "script 1"
    This reverts commit b67f45aa4ece69917d75ec0dcbbe5227421d5fef.

--- script1.py
+++ script1.py
@@ -1,4 +1,3 @@
-# print line of Zen of Python
 print ("There should be one-- and preferably only one --obvious wa to do it.")
 print ("Although that way may not be obvious at first unless you're Dutch")
 

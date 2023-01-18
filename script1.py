a5b41a4aaa7783c3abfc3ae2ea56adf7ce6269f9
matthewkuchurean <matthew.kuchurean@flemingcollege.ca>
2023-01-18 13:57:41 -0500

    script 1
    edit made

--- script1.py
+++ script1.py
@@ -1,3 +1,4 @@
+#print edit  
 print ("There should be one-- and preferably only one --obvious wa to do it.")
-print ("Although that way may not be obvious at first unless you're Dutch")
+
 
--- script2.py
+++ script2.py
@@ -1,3 +1,7 @@
+# print edits
+
 print ("There should be one-- and preferably only one --obvious wa to do it.")
 print ("Although that way may not be obvious at first unless you're Dutch")
 print ("Flat is better than nested")
+print ("There should be one-- and preferably only one --obvious wa to do it.")
+print ("Although that way may not be obvious at first unless you're Dutch")

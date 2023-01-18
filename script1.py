42a53fcec878c1d5489f6519749d25c43f7d1688
matthewkuchurean <91102676+matthewkuchurean@users.noreply.github.com>
2023-01-18 13:44:27 -0500

    script 1 change
    add extra line

--- script1.py
+++ script1.py
@@ -1,3 +1,4 @@
+# change of zen of python
 print ("There should be one-- and preferably only one --obvious wa to do it.")
 print ("Although that way may not be obvious at first unless you're Dutch")
 

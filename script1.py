b67f45aa4ece69917d75ec0dcbbe5227421d5fef
matthewkuchurean <matthew.kuchurean@flemingcollege.ca>
2023-01-18 13:42:02 -0500

    script 1
    print extra line

--- script1.py
+++ script1.py
@@ -1,3 +1,4 @@
+# print line of Zen of Python
 print ("There should be one-- and preferably only one --obvious wa to do it.")
 print ("Although that way may not be obvious at first unless you're Dutch")
 
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
+# print some of line of Zen of Python
 print ("There should be one-- and preferably only one --obvious wa to do it.")
 print ("Although that way may not be obvious at first unless you're Dutch")
 print ("Flat is better than nested")
